8673f918a1615fc11e0de0da6e7129fd2b9d5573
Patrick Gillan <pgillan@minorimpact.com>
2017-07-31 13:26:58 -0700

    Updated test script.  Should provide more stability and volatility into the graphs.

--- bin/test.pl
+++ bin/test.pl
@@ -1,19 +1,39 @@
 #!/usr/bin/perl
 
 use Time::HiRes qw(tv_interval gettimeofday);
+use Getopt::Long "HelpMessage";
+
+my $options = {
+                config => $ENV{MINORIMPACT_CONFIG},
+                help => sub { HelpMessage(); },
+            };
+
+Getopt::Long::Configure("bundling");
+GetOptions( $options,
+            "config|c=s",
+            "count",
+            "force|f",
+            "help|?|h",
+            "id|i=i",
+            "test_count",
+            "verbose",
+        ) || HelpMessage();
+
 
 use MinorImpact;
 use MinorImpact::InfluxDB;
 use MinorImpact::Object;
 use MinorImpact::Object::Search;
+use MinorImpact::Test;
 use MinorImpact::Util;
 
 use lib "../lib";
 use note;
 
-my $test_count = 5;
+die "config file $options->{config} does not exist" unless (-f $options->{config});
+my $test_count = $options->{test_count} || int(rand(10));
 my $MAX_CHILD_COUNT = 3;
-my $verbose = 0;
+my $verbose = $options->{verbose};
 my $start_time = [gettimeofday];
 
 my %child;
@@ -36,61 +56,89 @@ while ($i++ < $test_count) {
 while (wait() > 0) {}
 my $end_time = [gettimeofday];
 my $total_time = tv_interval($start_time, $end_time);
-my $avg_time = $total_time/$test_count;
-#print "total_time=$total_time\n";
-#print "avg_time=$avg_time\n";
-MinorImpact::InfluxDB::influxdb({ db => "note_stats", metric => "test_avg", value => $avg_time });
+if ($test_count) {
+    my $avg_time = $total_time/$test_count;
+    MinorImpact::InfluxDB::influxdb({ db => "note_stats", metric => "test_avg", value => $avg_time });
+}
 
 my $MINORIMPACT = new MinorImpact({ no_log => 1 });
 my $DB = $MinorImpact::SELF->{DB};
 my $USERDB = $MinorImpact::SELF->{USERDB};
 my $user_count = $USERDB->selectrow_array("SELECT count(*) FROM user");
 my $note_count = $DB->selectrow_array("SELECT count(*) FROM object WHERE object_type_id=?", undef, (MinorImpact::Object::typeID('note')));
-print "user_count=$user_count\n" if ($verbose);
+print "user_count=$user_count\n" if ($options->{verbose});
 MinorImpact::InfluxDB::influxdb({ db => "note_stats", metric => "user_count", value => $user_count });
-print "note_count=$note_count\n" if ($verbose);
+print "note_count=$note_count\n" if ($options->{verbose});
 MinorImpact::InfluxDB::influxdb({ db => "note_stats", metric => "note_count", value => $note_count });
 
 sub test {
     my $test_start_time = [gettimeofday];
+    srand();
     my $MINORIMPACT = new MinorImpact({ no_log => 1 });
-    my $password = time() . $$ . int(rand(100)) ;
-    my $username = "test_user_$password";
-    print "adding test_user $username\n" if ($verbose);
-    MinorImpact::User::addUser({ username => $username, password => $password });
-    my $user = MinorImpact::getUser({ username => $username, password => $password }) || die "Can't retrieve user $username\n";;
 
-#print "user name: " . $user->name() . "\n";
-#print "user id: " . $user->id() . "\n";
+    my $user = MinorImpact::Test::randomUser();
+    my $user_type = int(rand(4));
+    if ($user_type == 0 || !$user) { # new user
+        my $password = time() . $$ . int(rand(100)) ;
+        my $username = "test_user_note_$password";
+        print "$$ adding user $username\n" if ($options->{verbose});
+        MinorImpact::User::addUser({ username => $username, password => $password });
+        $user = MinorImpact::getUser({ username => $username, password => $password }) || die "Can't retrieve user $username\n";;
+    } elsif ($user_type == 1) { # angry user
+        print "$$ deleting user " . $user->name() . "(" . $user->id() . ")\n" if ($options->{verbose});
+        return $user->delete();
+    } else {
+        print "$$ logging in as " . $user->name() . "(" . $user->id() . ")\n" if ($options->{verbose});
+    }
 
-    my $tag = randomText(1);
-    my $note_count = 5; #int(rand(5)) + 1;
-#print "creating $note_count notes\n";
-    for (my $i = 0; $i < $note_count; $i++) {
-        my $text = randomText() . '.';
-        my $note = new note({ detail => $text, user_id => $user->id(), tags=>$tag });
-        if ($text ne $note->get('detail')) {
-#print "text mismatch\n";
+    while (my $action_type = int(rand(5))) {
+        my @notes = MinorImpact::Object::Search::search({ query => { 
+                                                            object_type_id => MinorImpact::Object::typeID('note'), 
+                                                            user_id => $user->id(),
+                                                        } });
+        my @tags;
+        my $note_count = scalar(@notes);
+        my $tag_count = scalar(@tags);
+        foreach my $note (@notes) {
+            push(@tags, $note->tags());
         }
-    }
-
-    #Search for all added notes.
-    my @notes = MinorImpact::Object::Search::search({ query => { 
-                                                        object_type_id => MinorImpact::Object::typeID('note'), 
-                                                        user_id => $user->id(),
-                                                    } });
-    if (scalar(@notes) != $note_count) {
-        #print "note count mismatch: '$note_count' != '" . scalar(@notes) . "'\n";
-    }
-
-    # Tag search
-    @notes = MinorImpact::Object::Search::search({ query => { 
-                                                        object_type_id => MinorImpact::Object::typeID('note'), 
-                                                        search => "tag:$tag",
-                                                        user_id => $user->id(),
-                                                    } });
-    if (scalar(@notes) != $note_count) {
-        #print "note count mismatch: '$note_count' != '" . scalar(@notes) . "'\n";
+        if ($action_type == 1 || $note_count == 0) { # add note
+            my $tag;
+            if (int(rand(3)) == 1 || $tag_count == 0) {
+                $tag = randomText(1);
+            } else {
+                $tag = $tags[int(rand($tag_count))];
+            }
+            my $text = randomText() . '.';
+            print "$$ adding note: $text tag: $tag\n" if ($options->{verbose});
+            my $note = new note({ detail => $text, user_id => $user->id(), tags=>$tag });
+            if ($text ne $note->get('detail')) {
+                print "$$ ERROR: text mismatch\n" if ($options->{verbose});
+            }
+        } elsif ($action_type == 2) { # delete note
+            my $note = $notes[int(rand($note_count))];
+            $note->delete();
+        } else { # search by tag and edit
+            my $tag = $tags[int(rand($tag_count))];
+            print "$$ searching for tag:$tag\n" if ($options->{verbose});
+            @notes = MinorImpact::Object::Search::search({ 
+                query => { 
+                object_type_id => MinorImpact::Object::typeID('note'), 
+                search => "tag:$tag",
+                user_id => $user->id(),
+            } });
+            if (scalar(@notes) == 0) {
+                print "$$ ERROR: no notes returned for tag:$tag\n" if ($options->{verbose});
+            } else {
+                my $note = $notes[int(rand(scalar(@notes)))];
+                my $text = randomText() . '.';
+                print "$$ updating note: $text\n" if ($options->{verbose});
+                $note->update({detail => $text});
+                if ($text ne $note->get('detail')) {
+                    print "$$ ERROR: text mismatch\n" if ($options->{verbse});
+                }
+            }
+        }
     }
     my $test_end_time = [gettimeofday];
     #print "test_time=" . tv_interval($test_start_time, $test_end_time) . "\n";
@@ -98,3 +146,5 @@ sub test {
     #$user->delete();
 }
 
+sub randomUser {
+}
